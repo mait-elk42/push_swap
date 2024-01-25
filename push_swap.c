cfaa65b87076dbf79535f126f8e6af3589146875
Mohamed Ait Elkadi <mait-elk@e3r4p5.1337.ma>
2024-01-25 17:10:27 +0100

    pb fixed

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 22:08:30 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/01/25 16:37:19 by mait-elk         ###   ########.fr       */
+/*   Updated: 2024/01/25 17:04:06 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,16 @@ void	_put_ab(t_nsx_node *a, t_nsx_node *b)
 	ft_printf("--------------------------\n");
 	while (a)
 	{
-			ft_printf("[A%d]\t", a->num);
-			a = a->next;
-		ft_printf("\n");
+		ft_printf("[%d]", a->num);
+		a = a->next;
 	}
-	ft_printf(" A\t B\n");
-	ft_printf("--------------------------\n");
-
-
+	ft_printf("\tA\n");
+	while (b)
+	{
+		ft_printf("[%d]", b->num);
+		b = b->next;
+	}
+	ft_printf("\tB\n");
 }
 
 int	main(int ac, char **av)
@@ -39,27 +41,15 @@ int	main(int ac, char **av)
 	a = NULL;
 	b = NULL;
 	 numbers = _nsx_get_list(ac, av);
-	// _nsx_args_checker(ac, av);
-	// _nsx_num_checker(ac, av);
-	// a = _nsx_2darr2list(numbers);
-	// _put_ab(a, b);
-	// _nsx_instr_pb(&a, &b);
-	// _nsx_instr_pb(&a, &b);
-	// _put_ab(a, b);
-	// _nsx_instr_pb(&a, &b);
-	// // _nsx_instr_sa(a);
-	// // _nsx_instr_pb(&a, &b);
-	// _nsx_exit("Great ! Let's Sort Numbers!!!", 0, 0);
-	_nsx_lstadd_atbegin(&a, _nsx_lstnew_node(3));
-	_nsx_lstadd_atbegin(&a, _nsx_lstnew_node(2));
-	_nsx_lstadd_atbegin(&a, _nsx_lstnew_node(1));
-	// _put_ab(a, b);
-	_nsx_instr_pb(&a, &b);
-	// _put_ab(a, b);
+	_nsx_args_checker(ac, av);
+	_nsx_num_checker(ac, av);
+	a = _nsx_2darr2list(numbers);
+	_put_ab(a, b);
 	_nsx_instr_pb(&a, &b);
 	_put_ab(a, b);
-	// printf("%d\n", a->num);
-	// printf("%d\n", b->num);
-	// printf("%d\n", b->next->num);
-	// printf("%d\n", a->next->num);
+	_nsx_instr_pb(&a, &b);
+	_put_ab(a, b);
+	_nsx_instr_pb(&a, &b);
+	// _put_ab(a, b);
+	_nsx_exit("Great ! Let's Sort Numbers!!!", 0, 0);
 }
--- utils/instr01.c
+++ utils/instr01.c
@@ -6,7 +6,7 @@
 /*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 22:57:35 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/01/25 16:32:57 by mait-elk         ###   ########.fr       */
+/*   Updated: 2024/01/25 17:08:37 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,29 @@ void	_nsx_instr_sb(t_nsx_node *b)
 
 void	_nsx_instr_ss(t_nsx_node *a, t_nsx_node *b)
 {
-	_nsx_instr_sa(a);
-	_nsx_instr_sb(b);
+	int tmp;
+
+	if (!a || !a->next)
+		return ;
+	tmp = a->next->num;
+	a->next->num = a->num;
+	a->num = tmp;
+
+	if (!b || !b->next)
+		return ;
+	tmp = b->next->num;
+	b->next->num = b->num;
+	b->num = tmp;
 	ft_printf("ss\n");
 }
 
 void	_nsx_instr_pb(t_nsx_node **a, t_nsx_node **b)
 {
-	t_nsx_node *first;
-	t_nsx_node *sec;
+	t_nsx_node	*node;
 
-	first = *a;
-	sec = (*a)->next;
-	first->next = NULL;
-	(*a) = sec;
-	_nsx_lstadd_atbegin(b, first);
-	// (*a) = (*a)->next;
+	node = *a;
+	(*a) = (*a)->next;
+	node->next = 0;
+	_nsx_lstadd_atbegin(b, node);
 	ft_printf("pb\n");
 }
