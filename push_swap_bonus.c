741ab3beecdc9de935d71be9e77f8595388cc251
Mohamed Ait Elkadi <mait-elk@e3r2p16.1337.ma>
2024-02-13 20:43:17 +0100

    finishshshshs

--- push_swap_bonus.c
+++ push_swap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 18:38:04 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/02/12 14:19:02 by mait-elk         ###   ########.fr       */
+/*   Updated: 2024/02/13 09:47:40 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,44 @@ int	is_valid_command(char	*cmd)
 
 void	_nsx_execute_command(char *cmd, t_nsx_node **a, t_nsx_node **b)
 {
-	if (ft_strnstr(cmd, "sa", 2))
+	if (ft_strnstr(cmd, "sa", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_sa(a);
-	if (ft_strnstr(cmd, "sb", 2) && _nsx_strlen(cmd) == 2)
+	if (ft_strnstr(cmd, "sb", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_sb(b);
-	if (ft_strnstr(cmd, "ss", 2))
+	if (ft_strnstr(cmd, "ss", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_ss(a, b);
-	if (ft_strnstr(cmd, "pa", 2))
+	if (ft_strnstr(cmd, "pa", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_pa(a, b);
-	if (ft_strnstr(cmd, "pb", 2))
+	if (ft_strnstr(cmd, "pb", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_pb(a, b);
-	if (ft_strnstr(cmd, "ra", 2))
+	if (ft_strnstr(cmd, "ra", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_ra(a);
-	if (ft_strnstr(cmd, "rb", 2))
+	if (ft_strnstr(cmd, "rb", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_rb(b);
-	if (ft_strnstr(cmd, "rr", 2))
+	if (ft_strnstr(cmd, "rr", 2) && ft_strlen(cmd) == 2)
 		_nsx_instr_rr(a, b);
-	if (ft_strnstr(cmd, "rra", 3))
+	if (ft_strnstr(cmd, "rra", 3) && ft_strlen(cmd) == 3)
 		_nsx_instr_rra(a);
-	if (ft_strnstr(cmd, "rrb", 3))
+	if (ft_strnstr(cmd, "rrb", 3) && ft_strlen(cmd) == 3)
 		_nsx_instr_rrb(b);
-	if (ft_strnstr(cmd, "rrr", 3))
+	if (ft_strnstr(cmd, "rrr", 3) && ft_strlen(cmd) == 3)
 		_nsx_instr_rrr(a, b);
 }
 
 void	_nsx_read_instractions(t_nsx_node **a, t_nsx_node **b, char **cmds)
 {
-	while (*cmds)
+	int	i;
+
+	i = 0;
+	if (!cmds)
+		return ;
+	while (cmds[i])
 	{
-		free(*cmds);
-		_nsx_execute_command(*cmds, a, b);
-		cmds++;
+		_nsx_execute_command(cmds[i], a, b);
+		free(cmds[i]);
+		i++;
 	}
+	free(cmds);
 }
 
 char	**_nsx_read_commands(void)
@@ -104,13 +110,12 @@ int	main(int ac, char **av)
 	numbers = _nsx_get_list(ac, av);
 	a = _nsx_2darr2list(numbers);
 	commands = _nsx_read_commands();
-	if (!commands && _is_sorted(a))
-		(_nsx_lstfree(a), _nsx_exit_error());
 	_nsx_read_instractions(&a, &b, commands);
 	if (_is_sorted(a) && !b)
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
 	_nsx_lstfree(a);
+	_nsx_lstfree(b);
 	return (0);
 }
